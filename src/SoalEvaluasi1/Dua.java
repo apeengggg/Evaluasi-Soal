e2a5a7693defb1adadc6ccb5185b8b07fecbb4c5
apeengggg <mirfanmanaf0804@gmail.com>
2021-03-16 09:09:51 +0700

    update validasi evaluasi 1 no 2

--- src/SoalEvaluasi1/Dua.java
+++ src/SoalEvaluasi1/Dua.java
@@ -4,36 +4,45 @@ public class Dua {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int a, b;
-			int m =0;
-			int x = 0;
-			double hsl=0, c, d=2, e=0;
-			System.out.print("Masukkan Bilangan Biner : ");
-			Scanner input = new Scanner(System.in);
-			a = input.nextInt();
-			System.out.print("Konversi ke Desimal : ");
-					hasil(a,m, x,hsl);
-					System.out.println();
-		}
-
-			static void hasil(int a, int m, int x,double hsl){
-			double c, d=2, e=0;
-			int b;
-			if(a>=10){
-			b = a%10;
+		int a, b;
+		int m =0;
+		int x = 0;
+		double result=0, c, d=2, e=0;
+		System.out.print("Masukkan Bilangan Biner : ");
+		Scanner input = new Scanner(System.in);
+		String z = input.nextLine();
+		System.out.print("Konversi ke Desimal : ");
+		if(z.length()!=8){
+			System.out.print("Bukan Binary 8bit" + "\n");
+		}else{
+			if(z.matches("[0-1]*")){
+				a = Integer.parseInt(z);
+				hasil(a,m, x,result);
+				System.out.println();
 			}else{
-			b = a;
-			}
-			if(a>0){
-			c = b * Math.pow(d,m);
-			hsl=hsl+c;
-			if(hsl>127){
-				hsl=hsl-255;
-			}
-			if(b==a)
-			System.out.print(hsl);
-			m++;
-			hasil(a/10,m, x,hsl);
+				System.out.print("Bukan binary yg valid" + "\n");
 			}
 		}
 	}
+
+		static void hasil(int a, int m, int x,double result){
+		double c, d=2, e=0;
+		int b;
+		if(a>=10){
+		b = a%10;
+		}else{
+		b = a;
+		}
+		if(a>0){
+		c = b * Math.pow(d,m);
+		result+=c;
+		if(result>127){
+			result-=255;
+		}
+		if(b==a)
+		System.out.print(result);
+		m++;
+		hasil(a/10,m, x,result);
+		}
+	}
+}
